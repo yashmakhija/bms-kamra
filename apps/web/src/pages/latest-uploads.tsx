e429d3bf3a5cfb7eeb1d8c88a93824dd30029cac
yashmakhija <yashkrmakhija@gmail.com>
2025-05-01 05:11:14 +0530

    remove hero from podcasts and upload video

--- apps/web/src/pages/latest-uploads.tsx
+++ apps/web/src/pages/latest-uploads.tsx
@@ -1,12 +1,12 @@
-import { Hero } from "../components/latest-upload/hero";
+// import { Hero } from "../components/latest-upload/hero";
 import LatestVideos from "../components/latest-upload/latest-video";
 import { latestShows } from "../data/upload";
 
 export default function LatestUploadsPage() {
   return (
-    <div className="bg-neutral-900">
-      <Hero />
-      <LatestVideos shows={latestShows} />
+    <div className="bg-[#111111] pt-20">
+      {/* <Hero /> */}
+      <LatestVideos title="Watch Latest Videos" shows={latestShows} />
     </div>
   );
 }
