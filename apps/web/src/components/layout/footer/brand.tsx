6cc5eef9a474ca0539e3c686cdc92baeb17f773d
yashmakhija <yashkrmakhija@gmail.com>
2025-05-01 16:13:45 +0530

    ui improvement footer

--- apps/web/src/components/layout/footer/brand.tsx
+++ apps/web/src/components/layout/footer/brand.tsx
@@ -1,17 +1,22 @@
 export function Brand() {
   return (
     <div className="flex flex-col">
-      <div className=" items-start gap-4">
+      <div className=" items-start gap-4 ">
         <img
-          src="/logo.png"
+          src="/main-logo.png"
           alt="The Kunal Kamra App"
           width={52}
           height={52}
-          className="rounded-lg"
+          className="rounded-lg sm:mx-auto lg:mx-0"
         />
-        <h2 className="text-white mt-6 text-xl font-medium leading-tight">
-          The Kunal Kamra App
+        <h2 className="justify-start mt-4 text-neutral-50 text-base font-bold leading-tight">
+          Kunal Kamra
         </h2>
+      </div>
+      <div className="self-stretch mt-2 justify-start text-neutral-50 text-sm font-normal">
+        Disclaimer: All content on this site is fictional and satirical,
+        <br />
+        intended purely for entertainment purposes.
       </div>
     </div>
   );
--- apps/web/src/components/layout/footer/index.tsx
+++ apps/web/src/components/layout/footer/index.tsx
@@ -8,27 +8,45 @@ export function Footer() {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="w-full bg-neutral-800 py-16">
-      <div className="container mx-auto px-4">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
-          <div className="space-y-8">
+    <footer className="w-full">
+      <div className="w-full bg-neutral-800 px-4 py-16">
+        <div className="container mx-auto">
+          {/* Mobile and Tablet View */}
+          <div className="flex flex-col items-center text-center lg:hidden">
             <Brand />
-            <div className="flex items-center mt-4 gap-4">
+
+            <div className="mt-4 mb-12">
               <SocialLinks />
             </div>
+
+            <div className="w-full grid grid-cols-1 gap-10">
+              <Navigate />
+              <Legal />
+              <Support />
+            </div>
           </div>
-          <div className="md:pl-12 grid grid-cols-1 md:grid-cols-3 gap-12">
-            <Navigate />
-            <Support />
-            <Legal />
+
+          {/* Desktop View */}
+          <div className="hidden lg:grid grid-cols-1 lg:grid-cols-2 gap-12">
+            <div className="space-y-8">
+              <Brand />
+
+              <div className="flex items-center mt-4 gap-4">
+                <SocialLinks />
+              </div>
+            </div>
+            <div className="lg:pl-12 grid grid-cols-3 gap-12">
+              <Navigate />
+              <Legal />
+              <Support />
+            </div>
           </div>
         </div>
-        <div className="w-full border-t border-neutral-700 my-6"></div>
-        <div className="pt-2 border-t border-neutral-800">
-          <p className="text-neutral-300 text-sm font-normal leading-none">
-            © {currentYear} Kunal Kamra. All Rights Reserved
-          </p>
-        </div>
+      </div>
+      <div className="w-full py-4 bg-[#f2f900] border-t border-[#1d1d1d] flex justify-center items-center">
+        <p className="justify-start text-[#1d1d1d] text-sm font-normal leading-none">
+          © {currentYear} Kunal Kamra. All Rights Reserved
+        </p>
       </div>
     </footer>
   );
